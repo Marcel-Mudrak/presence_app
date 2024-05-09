91e5301b09cab0856efd0fda642760684da0c9b9
Marcel-Mudrak <marcel-mudrak@wp.pl>
2024-05-09 21:08:42 +0200

    unused import

--- lib/screen/home/view/home_screen_view.dart
+++ lib/screen/home/view/home_screen_view.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:presence_app/common/constant/app_colors.dart';
 import 'package:presence_app/common/constant/app_images.dart';
